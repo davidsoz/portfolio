4442fdc7c064e8b188bbb0096727bced72bfc814
David Sozashvili <davidsozashvili@gmail.com>
2023-04-10 16:15:13 +0400

    add Gamma consulting in projects

--- src/componenets/projects/Projects.js
+++ src/componenets/projects/Projects.js
@@ -2,11 +2,13 @@
 import styled from 'styled-components';
 import { SingleProject } from './SingleProject';
 import cherrysLogo from "../../assets/cherysLogo.png";
+import gammaLogo from "../../assets/GC_Logo.png";
 
 export function Projects() {
     return (
         <Container>
             <SingleProject logoUrl={cherrysLogo} name="Cherry's fitness" url="https://www.cherrysfit.com/"/>
+            <SingleProject logoUrl={gammaLogo} name="Gamma Consulting" url="https://gamma.ge/en"/>
         </Container>
     )
 }
